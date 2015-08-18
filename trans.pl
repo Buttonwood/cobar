7f18b72dbcf5bddb42fe64330748fa72c68a3193
Buttonwood <tanhao2013@foxmail.com>
2015-08-18 15:17:57 +0800

    Update trans.pl

--- trans.pl
+++ trans.pl
@@ -1,7 +1,20 @@
 while(<>){
+        if(/^s*$/){
+                next;
+        }elsif(/^(\d+:\d+:\d+,\d+)/){
+                print "##########$_";
+        }else{
+                print "$_";
+        }
+}
+
+
+=pod
+while(<>){
         if(/^(\d+:\d+:\d+,\d+)/){
                 print "##$_";
         }else{
                 print "$_";
         }
 }
+=cut
